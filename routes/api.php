aa631bc76af4f030236c61c3a6ba167d99d9668c
Wildan <alnataraw@gmail.com>
2018-02-24 15:39:30 +0700

    Add some api testing

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('products', function () {
+    return response()->json(['productId' => 1], 200);
+});
